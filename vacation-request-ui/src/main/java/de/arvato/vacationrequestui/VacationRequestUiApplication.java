ef1dd07c31cdcb6e7fc5b05c63cf3986631bab38
Octavian Nitra <octavian.nitra@bertelsmann.de>
2020-06-30 16:12:55 +0300

    initial setup

---
+++ vacation-request-ui/src/main/java/de/arvato/vacationrequestui/VacationRequestUiApplication.java
@@ -0,0 +1,13 @@
+package de.arvato.vacationrequestui;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VacationRequestUiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(VacationRequestUiApplication.class, args);
+    }
+
+}
---
+++ vacation-request-ui/src/test/java/de/arvato/vacationrequestui/VacationRequestUiApplicationTests.java
@@ -0,0 +1,13 @@
+package de.arvato.vacationrequestui;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VacationRequestUiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
