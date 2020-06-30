ba635acfec2e967648fc06b2b63976946ff5e890
Octavian Nitra <octavian.nitra@bertelsmann.de>
2020-06-30 16:25:31 +0300

    initial version

---
+++ employee-module/src/main/java/de/arvato/employeemodule/EmployeeModuleApplication.java
@@ -0,0 +1,13 @@
+package de.arvato.employeemodule;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmployeeModuleApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EmployeeModuleApplication.class, args);
+    }
+
+}
---
+++ employee-module/src/test/java/de/arvato/employeemodule/EmployeeModuleApplicationTests.java
@@ -0,0 +1,13 @@
+package de.arvato.employeemodule;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EmployeeModuleApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
