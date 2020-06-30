4395347155533f3bcdebcd66c53d72e499616a09
Octavian Nitra <octavian.nitra@bertelsmann.de>
2020-06-30 16:22:39 +0300

    initial version

---
+++ vacation-request-module/src/main/java/de/arvato/vacationrequestmodule/VacationRequestModuleApplication.java
@@ -0,0 +1,13 @@
+package de.arvato.vacationrequestmodule;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VacationRequestModuleApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(VacationRequestModuleApplication.class, args);
+    }
+
+}
---
+++ vacation-request-module/src/test/java/de/arvato/vacationrequestmodule/VacationRequestModuleApplicationTests.java
@@ -0,0 +1,13 @@
+package de.arvato.vacationrequestmodule;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VacationRequestModuleApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
