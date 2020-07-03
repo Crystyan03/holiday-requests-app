ece0b366f89837deab03380d0e44d550fc9bdce7
Adrian Groza <adriangroza29@gmail.com>
2020-07-03 11:01:57 +0300

    Routing based on authentication. Added logout but not working yet.

--- vacation-request-ui/src/main/java/de/arvato/vacationrequestui/controller/HomeController.java
+++ vacation-request-ui/src/main/java/de/arvato/vacationrequestui/controller/HomeController.java
@@ -1,6 +1,7 @@
 package de.arvato.vacationrequestui.controller;
 
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
@@ -10,6 +11,9 @@ public class HomeController {
 
     @GetMapping("/")
     public String home() {
+        if (!SecurityContextHolder.getContext().getAuthentication().isAuthenticated()) {
+            return "redirect:/login";
+        }
         return "home";
     }
 }
--- vacation-request-ui/src/main/java/de/arvato/vacationrequestui/controller/LoginController.java
+++ vacation-request-ui/src/main/java/de/arvato/vacationrequestui/controller/LoginController.java
@@ -2,6 +2,7 @@ package de.arvato.vacationrequestui.controller;
 
 import de.arvato.vacationrequestui.security.model.User;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -15,6 +16,9 @@ public class LoginController {
 
 	@GetMapping()
 	public String current(Model model){
+		if (SecurityContextHolder.getContext().getAuthentication().isAuthenticated()) {
+			return "redirect:/";
+		}
 		model.addAttribute("user", new User());
 		return "login";
 	}
